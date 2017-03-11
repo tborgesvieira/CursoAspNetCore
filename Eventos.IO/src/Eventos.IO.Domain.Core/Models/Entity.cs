cc7fac749c04db1a056fa3a2f3286d23ee1fe3f1
Thiago Borges Vieira <thiago@upgradesolutions.com.br>
2017-03-11 07:51:19 -0300

    Projeto da aula - video parado em 49:35 aula 10

---
+++ Eventos.IO/src/Eventos.IO.Domain.Core/Models/Entity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Core.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator == (Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator != (Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            //907 - mas pode ser qualquer número, somente para gerar um número único
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + $"[Id = {Id}]";
+        }
+    }
+}
---
+++ Eventos.IO/src/Eventos.IO.Domain/Models/Evento.cs
@@ -0,0 +1,11 @@
+﻿using Eventos.IO.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Eventos.IO.Domain.Models
+{
+    public class Evento : Entity
+    {
+        //Parado no vídeo 10 em 49:35        
+    }
+}
